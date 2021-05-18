f6ff6adbd4374ae09d6582be591d43a91c1e1623
JoeBakata <jplayinggames+git@gmail.com>
2021-05-18 19:18:54 -0400

    first commit

---
+++ Utilities/Enum.cs
@@ -0,0 +1,69 @@
+﻿namespace DungeonsAndDragons.ChartEngine.Utilities
+{
+    public enum Dice
+    {
+        //todo  How to do multiple dice? say 2d4, 2d6, 2d8, d8+d12 etc.
+        D2,
+        D3,
+        D4,
+        D6,
+        D7,
+        D8,
+        D10,
+        D12,
+        D20,
+        D100,
+        DNull,
+        DTests
+    }
+
+    public enum MonsterType
+    {
+        A,
+        B,
+        C,
+        D,
+        E,
+        F,
+        G,
+        H,
+        I,
+        J,
+        K,
+        L,
+        M,
+        N,
+        O,
+        P,
+        Q,
+        R,
+        S,
+        T,
+        U,
+        V
+    }
+
+
+    public enum GemType
+    {
+        Quartz = 10,  
+        Turquoise = 10,
+        Citrine = 50,
+        Onyx = 50,
+        Amber = 100,
+        Garnet = 100,
+        Pearl = 500,
+        Topaz = 500,
+        Opal = 1000,
+        Ruby = 1000
+    }
+
+    public enum MonetaryTrasureTypes
+    {
+        Copper,
+        Silver,
+        Electrum,
+        Gold,
+        Platinum
+    }
+}
---
+++ test.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using DungeonsAndDragons.ChartEngine.Utilities;
+using DungeonsAndDragons.ChartEngine.Charts.Treasure;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonsAndDragons.ChartEngine
+{
+    class test
+    {
+
+         
+        /// <summary>
+        /// Display everything in monetary chart
+        /// </summary>
+        public static void Main()
+        {
+            Charts.GetCharts FirstTest;
+
+           
+
+            FirstTest = new Charts.GetCharts();
+
+            //Arrange
+            FirstTest.GetMonetaryChart();
+            //Act
+
+            //Assert
+            // string name = FirstTest.MonetaryTreasure.First(x => x.Key == Utilities.MonsterType.A).Value.Select(x => x.)
+
+            //todo 1 CW the following output: What dice is used for A silver, C platinum. Use a for loop because its specific. Silver and Platinum should be displayed.
+            //Not sure how to do this. Once I learn to do this I am positive I can figure out how to do the rest.
+            //todo 2 How to display C as well as B and A.
+            //todo 4 CW the Percentage of B for electrum.
+            List<MonitaryTreasure> Loot = FirstTest.MonetaryTreasure.First(x => x.Key == MonsterType.A).Value;
+
+            for (int i = 0; i < Loot.Count; i++)
+            {
+                if  //Not sure what the conditional should be. I think it should be an if statement
+                 { 
+                    Console.WriteLine($"The Die used for Chart A silver is {Dice.D6}");
+                 }
+
+            }
+
+
+            //foreach (var item in Loot)
+            //{
+                
+            //    Console.WriteLine($"The items in A are {item.TreasureName}");
+                
+            //}
+            Console.ReadLine();
+        }
+    }
+}
