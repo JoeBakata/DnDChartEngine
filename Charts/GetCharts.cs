6c10ac675951b9db360ab27898cd370044084c4e
MikeSheehe <msheehe1@aol.com>
2021-05-18 21:07:54 -0400

    Pulling Monetary Data From Text File.

--- Charts/GetCharts.cs
+++ Charts/GetCharts.cs
@@ -33,28 +33,60 @@ namespace DungeonsAndDragons.ChartEngine.Charts
             //MonetaryTreasure.Add(MonsterType.B, PopulateMonitaryChartB());
             //MonetaryTreasure.Add(MonsterType.C, PopulateMonitaryChartC());
 
-            DungeonsAndDragons.ChartEngine.Services.OpenFile services = new DungeonsAndDragons.ChartEngine.Services.OpenFile();
+            Services.OpenFile services = new Services.OpenFile();
             List<string> monetaryChartData = services.GetMonetaryData();
             string firstMonetaryChartData = monetaryChartData[0];
             var firstSplit = firstMonetaryChartData.Split(':');
-            var monsterType = firstSplit[0];
+            var monsterType = GetMonsterType(firstSplit[0]);
             var secondSplit = firstSplit[1].Split(';');
-            
 
+            MonetaryTreasure.Add(monsterType, PopulateMonitaryChart(secondSplit));
         }
 
-        public List<Treasure.MonitaryTreasure> PopulateMonitaryChart(string[]  secondSplit)
+        public List<Treasure.MonitaryTreasure> PopulateMonitaryChart(string[] secondSplit)
         {
             var TreasureRewards = new List<Treasure.MonitaryTreasure>();
             foreach (var element in secondSplit)
             {
-
+                var thirdSplit = element.Split(',');
+                TreasureRewards.Add(new Treasure.MonitaryTreasure(thirdSplit[0], getDice(thirdSplit[1]), getDice(thirdSplit[2]),
+                    Int16.Parse(thirdSplit[3]), double.Parse(thirdSplit[4])));
             }
-
-
 
             return TreasureRewards;
         }
+
+
+        private Dice getDice(string diceNumber)
+        {
+            switch(diceNumber)
+            {
+                case "D6":
+                    return Dice.D6;
+                case "D4":
+                    return Dice.D4;
+                case "DNull":
+                    return Dice.DNull;
+            }
+
+            return Dice.DNull;
+        }
+
+
+        private MonsterType GetMonsterType(string monsterType)
+        {
+            switch(monsterType)
+            {
+                case "A":
+                    return MonsterType.A;
+                case "B":
+                    return MonsterType.B;
+            }
+
+            return MonsterType.A;
+        }
+
+
 
 
         public List<Treasure.MonitaryTreasure> PopulateMonitaryChartA()
