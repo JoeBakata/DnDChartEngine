168aa268c110faf1d32b880a35eb4fce4653b7a7
JoeBakata <jplayinggames+git@gmail.com>
2021-06-04 18:32:14 -0400

    Pushed Tests changes.
    test changes.

--- test.cs
+++ test.cs
@@ -13,7 +13,7 @@ namespace DungeonsAndDragons.ChartEngine
 
          
         /// <summary>
-        /// Display everything in monetary chart
+        /// Display everything in monetary chart.
         /// </summary>
         public static void Main()
         {
@@ -30,11 +30,7 @@ namespace DungeonsAndDragons.ChartEngine
             //Assert
             // string name = FirstTest.MonetaryTreasure.First(x => x.Key == Utilities.MonsterType.A).Value.Select(x => x.)
 
-            //todo 1 CW the following output: What dice is used for A silver, C platinum. Use a for loop because its specific. Silver and Platinum should be displayed.
-            //Not sure how to do this. Once I learn to do this I am positive I can figure out how to do the rest.
-            //todo 2 How to display C as well as B and A.
-            //todo 4 CW the Percentage of B for electrum.
-            List<MonitaryTreasure> Loot = FirstTest.MonetaryTreasure.First(x => x.Key == MonsterType.A).Value;
+            List<MonetaryTreasure> Loot = FirstTest.MonetaryTreasure.First(x => x.Key == MonsterType.A).Value;
 
             //for (int i = 0; i < Loot.Count; i++)
             //{
