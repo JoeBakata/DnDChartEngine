cc68b19232b8b3f6070ed754eeafce1bfb154e9f
JoeBakata <jplayinggames+git@gmail.com>
2021-06-08 16:48:47 -0400

    JewelryTreasure
    Added some comments and questions to this file.

--- Charts/Treasure/JewelryTreasure.cs
+++ Charts/Treasure/JewelryTreasure.cs
@@ -1,9 +1,10 @@
-﻿using DungeonsAndDragons.ChartEngine.Utilities;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DungeonsAndDragons.ChartEngine.Utilities;
+
 
 namespace DungeonsAndDragons.ChartEngine.Charts.Treasure
 {
@@ -13,27 +14,26 @@ namespace DungeonsAndDragons.ChartEngine.Charts.Treasure
         /// <summary>
         /// Name of the treasure.
         /// </summary>
-        public string TreasureName { get; set; }
+        public string TreasureName { get; set; }//confused by this. If it is the TreasureName and it means all jewelry why call it TreasureName?
 
         /// <summary>
         /// Type of dice.
         /// </summary>
-        public Dice Dice { get; set; }
+        public Dice Dice { get; set; }//don't understand this. Where is this used?
 
         /// <summary>
         /// Value of jewelry
         /// </summary>
 
+        public int MinimumValue { get; set; } //Is this actually a value? Or is it the number of Jewelry pieces?
 
-        public int MinimumValue { get; set; }
+        public int MaximumValue { get; set; }// same as above, is it the value or the number of jewelry pieces? I think it is the number of pieces.
 
-        public int MaximumValue { get; set; }
+        public double Percent { get; set; }//this is the percentage chance of jewelry being in the treasure.
 
-        public double Percent { get; set; }
+        public int NumberOfDice { get; set; }//This is number of dice rolled to determine the MinimumValue
 
-        public int NumberOfDice { get; set; }
-
-        public int MaxRollValue { get; set; }
+        public int MaxRollValue { get; set; }//This is the highest number of items there can be of jewelry in the treasure.
 
 
         public JewelryTreasure (string treasureName, int minimumValue, int maximumValue, int numberOfDice, int maxRollValue)
