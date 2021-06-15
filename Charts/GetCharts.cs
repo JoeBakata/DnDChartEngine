2fdbec966987fc68b253e6589e5d1bf46a817be1
JoeBakata <jplayinggames+git@gmail.com>
2021-06-15 19:46:59 -0400

    GetCharts
    Populated the GetCharts with methods. Need to check the todo and finish this.

--- Charts/GetCharts.cs
+++ Charts/GetCharts.cs
@@ -14,13 +14,18 @@ namespace DungeonsAndDragons.ChartEngine.Charts
         Services.OpenFile services = new Services.OpenFile();
         #endregion Fields
 
-        public Dictionary<MonsterType, List<Treasure.MonetaryTreasure>> MonetaryTreasure = //todo What is this doing? It is more complicated than what I learned. I think it is making
-            new Dictionary<MonsterType, List<Treasure.MonetaryTreasure>>(); //todo a new dictionary with value monstertype and key of list? Please explain.
+        public Dictionary<MonsterTypes, List<Treasure.MonetaryTreasure>> MonetaryTreasure = //todo What is this doing? It is more complicated than what I learned. I think it is making
+            new Dictionary<MonsterTypes, List<Treasure.MonetaryTreasure>>(); //todo a new dictionary with value monstertype and key of list? Please explain.
 
         public List<Treasure.JewelryValue> JewelryGPValueChart = new List<Treasure.JewelryValue>();
 
+        public List<Treasure.GemValue> GemGPValueChart = new List<Treasure.GemValue>();
+
         public GetCharts() //todo Why is nothing in the body of this? Because there are no parameters? This is just a Method?
         {
+            GetMonetaryChart();
+            GetJewelryValueChart();
+            GetGemValueChart();
         }
 
         public void GetJewelryValueChart()
@@ -34,6 +39,14 @@ namespace DungeonsAndDragons.ChartEngine.Charts
                     Int32.Parse(firstJewelryValueData[1]), 
                     Int32.Parse(firstJewelryValueData[2])));
             }
+        }
+        //todo create Methods (that will open the text file to get the data) for the Gem and MagicItem charts.
+        //todo also create the text docs.
+        public void GetGemValueChart()
+        {
+            List<string> goldPieceValue = services.GetDataFile(@"Resources\GemValueData.txt");
+
+
         }
 
         /// <summary>
@@ -78,10 +91,10 @@ namespace DungeonsAndDragons.ChartEngine.Charts
         /// </summary>
         /// <param name="monsterType"></param>
         /// <returns></returns>
-        private MonsterType GetMonsterType(string monsterType)
+        private MonsterTypes GetMonsterType(string monsterType)
         {
 
-            return (MonsterType)Enum.Parse(typeof(MonsterType), monsterType);
+            return (MonsterTypes)Enum.Parse(typeof(MonsterTypes), monsterType);
         }
         #endregion Private Methods
         //todo How do we make MonetaryChartData.txt more human readable. I tried to do some research and I think you can put in spaces and then when it reads the file it can string.split
