cb77d50c1806b6306195c35db28adc13b8786b75
JoeBakata <jplayinggames+git@gmail.com>
2021-06-18 22:51:18 -0400

    JewelryValue
    Added a bunch of comments, summaries, and information.
    Still have questions.

--- Charts/Treasure/JewelryValue.cs
+++ Charts/Treasure/JewelryValue.cs
@@ -10,26 +10,47 @@ namespace DungeonsAndDragons.ChartEngine.Charts.Treasure
     public class JewelryValue
     {
         #region Properties
+
+        /// <summary>
+        /// The types of jewelry found in the treasure.
+        /// <example>crown, sceptre, necklace, etc.</example>
+        /// </summary>
         public JewelryType JewelryType { get; set; }
 
+        /// <summary>
+        /// Lowest gold piece value of the jewelry.
+        /// </summary>
         public int MinimumGPValue { get; set; }
 
+        /// <summary>
+        /// Highest gold piece value of the jewelry.
+        /// </summary>
         public int MaximumGPValue { get; set; }
 
         #endregion Properties
 
-        public JewelryValue(string jewelryType, int minimumGPValue, int maximumGPValue)
-        {
+        /// <summary>
+        /// Instanstiate JewelryValue. Instantiate literally means "to create an instance of". In programming, this generally means to create an instance of an object.
+        /// You do that by using a constructor. A constructor is a special method of a class that initializes an object of that class.
+        /// Constructors have the same name as the class they're in, and because they always return an instance of that class, a return type isn't needed.
+        /// </summary>
+        /// <param name="jewelryType">Types of jewelry found in the treasure.</param>
+        /// <param name="minimumGPValue">Lowest gold piece value of the jewelry.</param>
+        /// <param name="maximumGPValue">Highest gold piece value of the jewelry.</param>
+        public JewelryValue(string jewelryType, int minimumGPValue, int maximumGPValue)//JewelryValue constructor with arguements string jewelryType,
+        {                                               //int minimumGPValue, int maximumGPValue.
             JewelryType = GetJewelryType(jewelryType);
             MinimumGPValue = minimumGPValue;
             MaximumGPValue = maximumGPValue;
         }
-        private JewelryType GetJewelryType(string jewelryType)
+
+        #region Private Method
+        private JewelryType GetJewelryType(string jewelryType)//GetJewelryType Method with access modifier of private and return type of JewleryType.
         {
 
             return (JewelryType)Enum.Parse(typeof(JewelryType), jewelryType);
         }
-
+        #endregion Private Method
     }
 
 
