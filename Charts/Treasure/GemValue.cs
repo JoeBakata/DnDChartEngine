c725c22f37ccd8433f52907e6cdbc097f4c21cb3
JoeBakata <jplayinggames+git@gmail.com>
2021-06-18 22:50:54 -0400

    GemValue
    Added a bunch of comments, summaries, and information.
    Still have questions.

--- Charts/Treasure/GemValue.cs
+++ Charts/Treasure/GemValue.cs
@@ -10,19 +10,44 @@ namespace DungeonsAndDragons.ChartEngine.Charts.Treasure
     public class GemValue
     {
         #region Properties
+        /// <summary>
+        /// The type of Gem found in the treasure
+        /// <example>Topaz, Pearl, Ruby, etc.</example>
+        /// </summary>
         public GemType GemType { get; set; }
 
+        /// <summary>
+        /// The gems minimum value in gold pieces.
+        /// </summary>
         public int MinimumGPValue { get; set; }
 
+        /// <summary>
+        /// The minimum number of gems -- or so I think.
+        /// </summary>
         public int MinimumRollValue { get; set; }
 
+        /// <summary>
+        /// The maximum number of gems -- or so I think.
+        /// </summary>
         public int MaximumRollValue { get; set; }
 
+        /// <summary>
+        /// Refers to the Dice enum to get a dice type -- I think.
+        /// </summary>
         public Dice Dice { get; set; }
         #endregion Properties
 
-        public GemValue(string gemType, int minimumGPValue, int minimumRollValue, int maximumRollValue)
-        {
+        /// <summary>
+        /// Instantiate GemValue. Instantiate literally means "to create an instance of". In programming, this generally means to create an instance of an object.
+        /// You do that by using a constructor. A constructor is a special method of a class that initializes an object of that class.
+        /// Constructors have the same name as the class they're in, and because they always return an instance of that class, a return type isn't needed.
+        /// </summary>
+        /// <param name="gemType">Determines the types of gems in the treasure.</param>
+        /// <param name="minimumGPValue">Determines the lowest value, in gold pieces, of the gems -- I think.</param>
+        /// <param name="minimumRollValue">Not sure, see above.</param>
+        /// <param name="maximumRollValue">Not sure, see above.</param>
+        public GemValue(string gemType, int minimumGPValue, int minimumRollValue, int maximumRollValue)//GemValue constructor with arguements int minimumGPValue, int minimumRollValue,
+        {                                                                                              // and maximumRollValue.
             GemType = GetGemType(gemType);
             MinimumGPValue = minimumGPValue;
             MinimumRollValue = minimumRollValue;
